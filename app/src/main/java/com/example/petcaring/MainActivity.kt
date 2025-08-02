02d88cd26052c2bd41d5879ba8ad45de93e2188e
RoshanKER <roshandavis012@gmail.com>
2025-08-03 03:10:29 +1000

    corrected the warnings

--- app/src/main/java/com/example/petcaring/MainActivity.kt
+++ app/src/main/java/com/example/petcaring/MainActivity.kt
@@ -6,17 +6,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.petcaring.ui.auth.LoginScreen
 import com.example.petcaring.ui.auth.SignupScreen
 import com.example.petcaring.ui.theme.PetCaringTheme
