d16589565f44ac6f6e0b9973bd4e58f5b406f15a
RoshanKER <roshandavis012@gmail.com>
2025-08-03 20:25:52 +1000

    login

--- app/src/main/java/com/example/petcaring/MainActivity.kt
+++ app/src/main/java/com/example/petcaring/MainActivity.kt
@@ -78,7 +78,13 @@ class MainActivity : ComponentActivity() {
                                 onNavigateToLogin = { currentScreen = AuthScreen.LOGIN }
                             )
 
-                            
+                            AuthScreen.HOME -> {
+                                val user = authViewModel.loggedInUser.collectAsState().value
+                                user?.let {
+                                    val petViewModel: PetViewModel = viewModel()
+                                    petViewModel.userId = it.id
+                                    MainScreen(userId = it.id)
+                                }
                             }
                         }
                     }
